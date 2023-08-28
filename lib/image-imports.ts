b8ade97cb9c6301c3a8251385034413dcb1adfe5
edsattar <ahmed.sattar@live.com>
2023-08-28 21:58:45 +0600

    update

--- lib/image-imports.ts
+++ lib/image-imports.ts
@@ -1,14 +1,8 @@
-import front_desk from "@/public/front_desk.jpg";
-import lobby_a from "@/public/lobby_a.jpg";
-import lobby_b from "@/public/lobby_b.jpg";
-import restaurant_a from "@/public/restaurant_a.jpg";
-import restaurant_b from "@/public/restaurant_b.jpg";
-import building_front from "@/public/building_front.jpg";
 import { StaticImageData } from "next/image";
 
 type HeroCarouselSlide = {
   id: number;
-  src: StaticImageData;
+  src: string;
   alt: string;
   subtitle: string;
   description: string;
@@ -17,42 +11,42 @@ type HeroCarouselSlide = {
 export const hero_carousel_slides: HeroCarouselSlide[] = [
   {
     id: 1,
-    src: front_desk,
+    src: "/front_desk.jpg",
     alt: "front_desk",
     subtitle: "front_desk",
     description: "front_desk",
   },
   {
     id: 2,
-    src: lobby_a,
+    src: "/lobby_a.jpg",
     alt: "lobby_a",
     subtitle: "lobby_a",
     description: "lobby_a",
   },
   {
     id: 3,
-    src: lobby_b,
+    src: "/lobby_b.jpg",    
     alt: "lobby_b",
     subtitle: "lobby_b",
     description: "lobby_b",
   },
   {
     id: 4,
-    src: restaurant_a,
+    src: "/restaurant_a.jpg",
     alt: "restaurant_a",
     subtitle: "restaurant_a",
     description: "restaurant_a",
   },
   {
     id: 5,
-    src: restaurant_b,
+    src: "/restaurant_b.jpg",
     alt: "restaurant_b",
     subtitle: "restaurant_b",
     description: "restaurant_b",
   },
   {
     id: 6,
-    src: building_front,
+    src: "/building_front.jpg",
     alt: "building_front",
     subtitle: "building_front",
     description: "building_front",
