b7de6575812e36f534e67acfe19f9a47451e2191
edsattar <ahmed.sattar@live.com>
2023-10-03 13:51:48 +0600

    .

--- lib/bcrypt.ts
+++ lib/bcrypt.ts
@@ -1,9 +1,9 @@
 import bcrypt from "bcrypt";
 
-export function hashPass(password) {
+export function hashPass(password: string) {
   return bcrypt.hash(password, 10);
 }
 
-export function comparePass(password, hash) {
+export function comparePass(password: string, hash: string) {
   return bcrypt.compare(password, hash);
 }
